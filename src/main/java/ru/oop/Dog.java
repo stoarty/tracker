2ba0229fb0ef757dfdd245f28f7a9134ae4064aa
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-04 23:07:00 +0300

    7.2 Создание объекта

---
+++ src/main/java/ru/oop/Dog.java
@@ -0,0 +1,10 @@
+package ru.oop;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
