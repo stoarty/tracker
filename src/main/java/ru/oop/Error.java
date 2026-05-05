8732c69df839e0bbbc712f8984d8ce26f40b2073
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-05 11:34:20 +0300

    7.8 Конструктор

---
+++ src/main/java/ru/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error2 = new Error(false, 404, "Not Found");
+        error2.printInfo();
+        Error error3 = new Error(true, 500, "Internal Server Error");
+        error3.printInfo();
+    }
+}
