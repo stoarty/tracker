2c6a3f9c8a3062f76b932e2f98f559570132002e
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-08 18:38:08 +0300

    9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,14 +14,28 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                return items[i];
             }
         }
+        return null;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        System.arraycopy(items, 0, rsl, 0, size);
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] temp = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                temp[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(temp, count);
     }
 }
