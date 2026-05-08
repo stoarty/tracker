09cacad593121d44f3786dddf5aeb8ee616ea984
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-08 23:44:53 +0300

    9.9 Метод удаления заявки Tracker.delete

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -51,4 +51,14 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[--size] = null;
+            return true;
+        }
+        return false;
+    }
 }
