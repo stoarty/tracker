00454474fa8e94df87f27fe38ae2737d18e3f451
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-08 18:17:38 +0300

    fix: 8.7 correct field type

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -1,12 +1,13 @@
 package ru.tracker;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        var item = new Item();
-        var formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        var created = item.getCreated();
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        LocalDateTime created = item.getCreated();
         System.out.println(created.format(formatter));
     }
 }
